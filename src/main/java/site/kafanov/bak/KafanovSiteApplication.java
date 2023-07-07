5d2256fc56e57440406793cbf2a32422f90b6ffd
tunec <ekafanov@yandex.ru>
2023-07-07 17:23:26 +0600

    init

---
+++ src/main/java/site/kafanov/bak/KafanovSiteApplication.java
@@ -0,0 +1,13 @@
+package site.kafanov.bak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafanovSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafanovSiteApplication.class, args);
+    }
+
+}
